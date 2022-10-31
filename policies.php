a770e7a8f9682cacf4a15cce7ce6f879c7c9301d
DrielySantos <santosdriely98@gmail.com>
2022-10-31 15:53:44 -0300

    ajustes

---
+++ policies.php
@@ -0,0 +1,16 @@
+<?php
+
+require('config.php');
+
+$page_title = 'Privacidade';
+
+require('header.php');
+?>
+
+<h2>Página modelo</h2>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, sint sequi nam tempora quis doloremque cupiditate eos quaerat nulla laudantium perspiciatis. Nisi esse commodi ipsam nostrum fuga omnis iure quos.</p>
+
+<?php
+
+require('footer.php');
+?>
